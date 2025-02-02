65979eafdaae43b042a8e767a85abb1d0c2ee9dc
asgerami <amiraman467@gmail.com>
2025-02-02 19:10:43 +0300

    Add Logo component and integrate it into the authentication layout

---
+++ components/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Banknote } from "lucide-react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <Banknote className="storke h-11 w-11 stroke-green-600 stroke-[1.5] " />
+      <p className="bg-gradient-to-r from-green-400 to-green-600 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        SaveMate
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
