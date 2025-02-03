088832fc76872ee16a1b94c59760c720905ea264
asgerami <amiraman467@gmail.com>
2025-02-03 15:54:50 +0300

    Add ThemeSwitcherBtn component and RootProviders for theme management

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { ReactNode, useEffect, useState } from "react";
+
+function RootProviders({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <>{children}</>;
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default RootProviders;
